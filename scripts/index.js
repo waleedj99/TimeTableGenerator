dacb254fb710c1dc5f3e0e60b8132e27b8565946
waleedj99 <waleedj1699@gmail.com>
2019-10-26 14:44:04 +0530

    added html pages

--- scripts/index.js
+++ scripts/index.js
@@ -16,13 +16,13 @@ function addCourse(divName,addBtn){
     if (addBtn.className.includes("glyphicon-plus")) {
         addBtn.className = "input-group-append  glyphicon glyphicon-minus"
         var newDiv = document.createElement('div')
-        newDiv.innerHTML = ` <div class="form-group" id="courses-" `+ formLength + `>
+        newDiv.innerHTML = ` <div class="form-group" id="courses-`+ formLength + `">
                     <div class="col-lg-12 ">
                         <div class="input-group form-group">
                             <input type="text" class="input-group-addon form-control" id="courses">
                                 <span class="input-group-addon">
                                     <span>
-                                        <span  onclick="addDiv('courses',this)" class="input-group-append glyphicon glyphicon-plus" id=`+ formLength++ +` />
+                                        <span  onclick="addCourse('courses',this)" class="input-group-append glyphicon glyphicon-plus" id=`+ formLength++ +` />
                                     </span>
                                 </span>
                             </input>
@@ -32,4 +32,34 @@ function addCourse(divName,addBtn){
         document.getElementById(divName+'-main').appendChild(newDiv)
     }
     
+}
+
+
+function addInfo(divName, addBtn) {
+    var formLength = document.getElementById(divName + '-main').childElementCount
+    if (addBtn.className.includes("glyphicon-minus")) {
+        addBtn.parentNode.parentNode.parentNode.parentNode.removeChild(addBtn.parentNode.parentNode.parentNode)
+    }
+    if (addBtn.className.includes("glyphicon-plus")) {
+        addBtn.className = "input-group-append  glyphicon glyphicon-minus"
+        var newDiv = document.createElement('div')
+        newDiv.className =" form-group"
+        newDiv.id = 'sinfo-'+formLength
+        newDiv.innerHTML = `
+                    <div class="col-lg-12 ">
+                        <div class="input-group form-group">
+                            <input placeholder = "Year" type="text" class="input-group-addon form-control" id="courses">
+                            <input placeholder="Section" type="text" class="input-group-addon form-control" id="courses">
+                            <span class="input-group-addon">
+                                <span>
+                                    <span onclick="addInfo('sinfo',this)"
+                                        class="input-group-append glyphicon glyphicon-plus" id=`+ formLength++ +`" />
+                                </span>
+                            </span>
+                            </input>
+                        </div>
+                    </div>`
+        document.getElementById(divName + '-main').appendChild(newDiv)
+    }
+
 }
