77fff116a3fe56272bac5c4dce359d3400691248
Aftaab Zia <aftaab1999@gmail.com>
2020-06-08 21:10:10 +0530

    Added constraint where only certain teachers can teach certain subjects

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-from flask import Flask, request, render_template, jsonify
-from collections import OrderedDict 
+from flask import Flask, request, render_template
+from collections import OrderedDict
 import json
-import random
-import itertools
-from flask import send_file , make_response
-from flask_cors import CORS, cross_origin
-app = Flask(__name__)
-CORS(app)
+from timetable import GeneticAlgorithm
 
+app = Flask(__name__, static_folder='./TimeTableGen_FE/TimetableFE/time-table/build/static',
+            template_folder='./TimeTableGen_FE/TimetableFE/time-table/build')
 
-def _build_cors_prelight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.mimetype = "application/pdf"
-    return response
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -28,39 +14,42 @@ def home():
     else:
         pass
 
+
 @app.route('/g', methods=['GET', 'POST'])
 def get():
     if request.method == 'POST':
-        
-        from timetable import GeneticAlgorithm
         main_json = request.get_json()
-        
+
         ga = GeneticAlgorithm(main_json['no_days'], main_json['no_classes'])
-        fittest = ga.run_algorithm(main_json['days_list'], main_json['time_list'], main_json['section_list'], main_json['subject_list'], ['400', '401', '402', '404', '405'], ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10'])
+        fittest = ga.run_algorithm(main_json['days_list'], main_json['time_list'], main_json['subject_list'],
+                                   main_json['rooms'], main_json['teacher_list'], main_json['student_groups'])
         print(fittest)
         main_dic = OrderedDict()
-        class_dic = OrderedDict()  
+        class_dic = OrderedDict()
         for clname in fittest.std_grp_list:
-            #print(clname)
+            # print(clname)
             class_dic[clname] = OrderedDict()
             for day in fittest.day_list:
-                #day_dic[day] = period_dic
-                #print(day)
+                # day_dic[day] = period_dic
+                # print(day)
                 class_dic[clname][day] = OrderedDict()
                 for pnum in fittest.class_timings_list:
-                    #print(pnum)
-                    class_dic[clname][day][pnum]=OrderedDict()
-                    #period_dic[pnum] = {}
+                    # print(pnum)
+                    class_dic[clname][day][pnum] = OrderedDict()
+                    # period_dic[pnum] = {}
 
-        for index1,day in enumerate(fittest.timetable): 
-            for index2,period in enumerate(day):
+        for index1, day in enumerate(fittest.timetable):
+            for index2, period in enumerate(day):
                 for cl in period:
-                    subject,clname,teacher,room = str(cl).split(' ')[0:4]
-                    class_dic[clname][fittest.day_list[index1]][fittest.class_timings_list[index2]] = {'subject':subject,'teacher':teacher,'room':room}
+                    subject, clname, teacher, room = str(cl).split(' ')[0:4]
+                    class_dic[clname][fittest.day_list[index1]][fittest.class_timings_list[index2]] = {
+                        'subject': subject, 'teacher': teacher, 'room': room}
         main_dic["time_table"] = class_dic
         main_dic["days"] = fittest.day_list
         main_dic["times"] = fittest.class_timings_list
         print(main_dic)
-        return(json.dumps(main_dic))
+        return json.dumps(main_dic)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- timetable.py
+++ timetable.py
@@ -10,6 +10,9 @@ class Room:
     def __hash__(self):
         return hash(self.name)
 
+    def __eq__(self, other):
+        return self.name == other.name
+
     def __repr__(self):
         return str(self.name)
 
@@ -20,6 +23,9 @@ class Teacher:
 
     def __hash__(self):
         return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
 
     def __repr__(self):
         return str(self.name)
@@ -32,47 +38,72 @@ class Subject:
     def __hash__(self):
         return hash(self.name)
 
+    def __eq__(self, other):
+        return self.name == other.name
+
     def __repr__(self):
         return str(self.name)
 
     @staticmethod
     def random_subject(classes_per_sub, subject_list, class_times_list, day_list):
-        prob_not_empty = (classes_per_sub*len(subject_list)) / \
-            ((len(class_times_list)*len(day_list)))
+        prob_not_empty = (classes_per_sub * len(subject_list)) / \
+                         (len(class_times_list) * len(day_list))
         if random.uniform(0, 1) < prob_not_empty:
             return Subject(random.choice(subject_list))
         else:
             return Subject("empty")
 
 
-class Class:
-    def __init__(self, std_grp=None, subject=None, teacher=None, room=None):
-        self.std_grp = std_grp
-        self.subject = subject
-        self.teacher = teacher
-        self.room = room
+class StudentGroup:
 
-    def __str__(self):
-        return str(self.subject)+" "+str(self.std_grp)+" "+str(self.teacher)+" "+str(self.room)
+    def __init__(self, name, subject_teacher_map):
+        self.subject_teacher_map = subject_teacher_map
+        self.group_name = name
+
+    def __hash__(self):
+        return hash(self.group_name)
+
+    def __getitem__(self, item):
+        return self.subject_teacher_map[item]
+
+    def __eq__(self, other):
+        return self.group_name == other.group_name
 
     def __repr__(self):
-        return str(self.subject)+" "+str(self.std_grp)+" "+str(self.teacher)+" "+str(self.room)
+        return str(self.group_name)
+
+
+class Class:
+    def __init__(self, subject=None, room=None, student_group=None):
+        self.subject = subject
+        self.teacher = student_group[subject]
+        self.room = room
+        self.student_group = student_group
+
+    def __str__(self):
+        return str(self.subject) + " " + str(self.student_group) + " " + str(self.teacher) + " " + str(self.room)
+
+    def __repr__(self):
+        return str(self.subject) + " " + str(self.student_group) + " " + str(self.teacher) + " " + str(self.room)
 
 
 class TimeTable:
-    def __init__(self, day_list, class_times_list, std_grp_list, room_list, subject_list, teacher_list):
+    def __init__(self, day_list, class_times_list, room_list, subject_list, teacher_list, student_groups):
         self.n_days = len(day_list)
         self.n_class_per_day = len(class_times_list)
-        self.n_std_grps = len(std_grp_list)
 
         self.per_subject_count = 3
         self.day_list = day_list
         self.class_timings_list = class_times_list
-        self.std_grp_list = std_grp_list
         self.room_list = room_list
         self.subject_list = subject_list
         self.teacher_list = teacher_list
-        self.timetable = [[[Class(std_grp=std_grp_list[stg], subject=Subject.random_subject(self.per_subject_count, self.subject_list, self.class_timings_list, self.day_list), teacher=self.random_teacher(teacher_list), room=self.random_room(room_list)) for stg in range(self.n_std_grps)]
+        self.student_groups = [StudentGroup(str(i), stg) for i, stg in enumerate(student_groups)]
+        self.timetable = [[[Class(student_group=stg,
+                                  subject=Subject.random_subject(self.per_subject_count, self.subject_list,
+                                                                 self.class_timings_list, self.day_list),
+                                  room=self.random_room(room_list)) for stg
+                            in self.student_groups]
                            for j in range(self.n_class_per_day)] for i in range(self.n_days)]
 
     @staticmethod
@@ -86,22 +117,24 @@ class TimeTable:
     def get_conflicts(self):
         room_conflicts = 0
         teacher_conflicts = 0
+
         for i in range(self.n_days):
             for j in range(self.n_class_per_day):
                 room_set = set()
                 teacher_set = set()
-                for k in range(self.n_std_grps):
+                for k in range(len(self.student_groups)):
                     if self.timetable[i][j][k].subject.name != 'empty' and self.timetable[i][j][k].room in room_set:
                         room_conflicts += 1
                     else:
                         room_set.add(self.timetable[i][j][k].room)
-                    if self.timetable[i][j][k].subject.name != 'empty' and self.timetable[i][j][k].teacher in teacher_set:
+                    if self.timetable[i][j][k].subject.name != 'empty' and self.timetable[i][j][
+                        k].teacher in teacher_set:
                         teacher_conflicts += 1
                     else:
                         teacher_set.add(self.timetable[i][j][k].teacher)
         subjects_conflicts = 0
 
-        for k in range(self.n_std_grps):
+        for k in range(len(self.student_groups)):
             sc = 0
             subject_count = {}
             for i in range(self.n_days):
@@ -110,15 +143,26 @@ class TimeTable:
                         subject_count[self.timetable[i][j][k].subject.name] = 1
                     else:
                         subject_count[self.timetable[i]
-                                      [j][k].subject.name] += 1
+                        [j][k].subject.name] += 1
             for subject_name, subject_freq in subject_count.items():
 
                 if subject_name != 'empty':
                     # print('Subject={}, freq={}'.format(subject_name, subject_freq))
-                    sc = sc + abs(subject_freq-self.per_subject_count)
+                    sc = sc + abs(subject_freq - self.per_subject_count)
             subjects_conflicts += sc
-        print('Subject conflicts={}, room conflicts={}, teacher conflicts={}'.format(
-            subjects_conflicts, room_conflicts, teacher_conflicts))
+        teacher_student_group_conflicts = 0
+        for k in range(len(self.student_groups)):
+            for i in range(self.n_days):
+                for j in range(self.n_class_per_day):
+                    cur_sub = self.timetable[i][j][k].subject
+                    cur_teacher = self.timetable[i][j][k].teacher
+                    if cur_sub.name != 'empty' and self.student_groups[k][cur_sub] != cur_teacher:
+                        teacher_student_group_conflicts += 1
+
+        # print(
+        #     'Subject conflicts={}, room conflicts={}, teacher conflicts={}, Teacher-student group conflicts={}'.format(
+        #         subjects_conflicts, room_conflicts, teacher_conflicts, teacher_student_group_conflicts))
+        return subjects_conflicts + room_conflicts + teacher_conflicts + teacher_student_group_conflicts
 
     def get_fitness(self):
         """
@@ -127,67 +171,58 @@ class TimeTable:
         1. Two student groups occupying the same room at the same time.
         2. One teacher teaching two sets of student groups at the same time.
         """
-        conflicts = 0
-        room_conflicts = 0
-        teacher_conflicts = 0
-        for i in range(self.n_days):
-            for j in range(self.n_class_per_day):
-                room_set = set()
-                teacher_set = set()
-                for k in range(self.n_std_grps):
-                    if self.timetable[i][j][k].subject.name != 'empty' and self.timetable[i][j][k].room in room_set:
-                        room_conflicts += 1
-                    else:
-                        room_set.add(self.timetable[i][j][k].room)
-                    if self.timetable[i][j][k].subject.name != 'empty' and self.timetable[i][j][k].teacher in teacher_set:
-                        teacher_conflicts += 1
-                    else:
-                        teacher_set.add(self.timetable[i][j][k].teacher)
-        subjects_conflicts = 0
-
-        for k in range(self.n_std_grps):
-            sc = 0
-            subject_count = {}
-            for i in range(self.n_days):
-                for j in range(self.n_class_per_day):
-                    if not subject_count.get(self.timetable[i][j][k].subject.name):
-                        subject_count[self.timetable[i][j][k].subject.name] = 1
-                    else:
-                        subject_count[self.timetable[i]
-                                      [j][k].subject.name] += 1
-            for subject_name, subject_freq in subject_count.items():
-
-                if subject_name != 'empty':
-                    # print('Subject={}, freq={}'.format(subject_name, subject_freq))
-                    sc = sc + abs(subject_freq-self.per_subject_count)
-            subjects_conflicts += sc
-        conflicts = subjects_conflicts+room_conflicts+teacher_conflicts
-        # print('Subject conf={}, room con={}, teacher conf={}'.format(subjects_conflicts, room_conflicts, teacher_conflicts))
+        conflicts = self.get_conflicts()
         if conflicts == 0:
             return math.inf
         else:
-            return 1/conflicts
+            return 1 / conflicts
 
     def mutate(self):
-        MUTATION_RATE = 0.05
+        mutation_rate = 0.04
         for i in range(self.n_days):
             for j in range(self.n_class_per_day):
-                for k in range(self.n_std_grps):
+                for k in range(len(self.student_groups)):
 
-                    if(random.uniform(0, 1) < MUTATION_RATE):
+                    if random.uniform(0, 1) < mutation_rate:
                         self.timetable[i][j][k].room = Room(
                             random.choice(self.room_list))
 
-                    if(random.uniform(0, 1) < MUTATION_RATE):
-                        self.timetable[i][j][k].teacher = Teacher(
-                            random.choice(self.teacher_list))
-
-                    if(random.uniform(0, 1) < MUTATION_RATE):
+                    if random.uniform(0, 1) < mutation_rate:
                         self.timetable[i][j][k].subject = Subject.random_subject(
                             self.per_subject_count, self.subject_list, self.class_timings_list, self.day_list)
+                        self.timetable[i][j][k].teacher = self.timetable[i][j][k].student_group[
+                            self.timetable[i][j][k].subject]
 
 
-# TODO: Start from here tmrw
+def crossover(timetable1: TimeTable, timetable2: TimeTable):
+    # Randomly copy over some genes from either chromosome and return a new one
+    timetable_crossed = TimeTable(
+        timetable1.day_list, timetable1.class_timings_list, timetable1.room_list,
+        timetable1.subject_list, timetable1.teacher_list, timetable1.student_groups)
+    for i in range(timetable1.n_days):
+        for j in range(timetable1.n_class_per_day):
+            for k in range(len(timetable1.student_groups)):
+                if bool(random.getrandbits(1)):
+                    timetable_crossed.timetable[i][j][k].room = timetable1.timetable[i][j][k].room
+                else:
+                    timetable_crossed.timetable[i][j][k].room = timetable2.timetable[i][j][k].room
+
+                if bool(random.getrandbits(1)):
+                    timetable_crossed.timetable[i][j][k].subject = timetable1.timetable[i][j][k].subject
+                    timetable_crossed.timetable[i][j][k].teacher = timetable1.timetable[i][j][k].student_group[
+                        timetable1.timetable[i][j][k].subject]
+                else:
+                    timetable_crossed.timetable[i][j][k].subject = timetable2.timetable[i][j][k].subject
+                    timetable_crossed.timetable[i][j][k].teacher = timetable2.timetable[i][j][k].student_group[
+                        timetable2.timetable[i][j][k].subject]
+
+                # if bool(random.getrandbits(1)):
+                #     timetable_crossed.timetable[i][j][k].teacher = timetable1.timetable[i][j][k].teacher
+                # else:
+                #     timetable_crossed.timetable[i][j][k].teacher = timetable2.timetable[i][j][k].teacher
+    return timetable_crossed
+
+
 class GeneticAlgorithm:
 
     def __init__(self, n_days, n_class_per_day):
@@ -198,9 +233,9 @@ class GeneticAlgorithm:
         self.n_generations = 100
         self.crossover_prob = 0.75
 
-    def generate_init_population(self, day_list, class_list, std_grp_list, subject_list, room_list, teacher_list):
-        self.population = [TimeTable(day_list, class_list, std_grp_list, room_list,
-                                     subject_list, teacher_list) for i in range(self.population_size)]
+    def generate_init_population(self, day_list, class_list, subject_list, room_list, teacher_list, student_groups):
+        self.population = [TimeTable(day_list, class_list, room_list,
+                                     subject_list, teacher_list, student_groups) for i in range(self.population_size)]
 
     def get_fittest(self):
         max_fitness = -math.inf
@@ -239,21 +274,25 @@ class GeneticAlgorithm:
         avg = 0
         for t in self.population:
             avg += t.get_fitness()
-        return avg/self.population_size
+        return avg / self.population_size
 
-    def run_algorithm(self, day_list, class_list, std_grp_list, subject_list, room_list, teacher_list):
+    def run_algorithm(self, day_list, class_list, subject_list, room_list, teacher_list, student_group_map_list):
+        student_groups = []
+        for stg_map in student_group_map_list:
+            item = {Subject(s): Teacher(t) for s, t in stg_map.items()}
+            student_groups.append(item)
+
         self.generate_init_population(
-            day_list, class_list, std_grp_list, subject_list, room_list, teacher_list)
+            day_list, class_list, subject_list, room_list, teacher_list, student_groups)
         for g in range(self.n_generations):
             # Selection
-            new_population = []
-            new_population.append(self.get_fittest())
-            while len(new_population)<self.population_size:
+            new_population = [self.get_fittest()]
+            while len(new_population) < self.population_size:
                 parent1 = self.get_fittest()
                 parent2 = self.get_second_fittest()
                 # Crossover
-                if(random.uniform(0, 1) < self.crossover_prob):
-                    offspring = self.crossover(parent1, parent2)
+                if random.uniform(0, 1) < self.crossover_prob:
+                    offspring = crossover(parent1, parent2)
                 else:
                     offspring = copy.deepcopy(parent1)
                 # Mutation
@@ -262,7 +301,7 @@ class GeneticAlgorithm:
 
             self.population = new_population
             f = self.get_fittest().get_fitness()
-            
+
             print('Generation {}. Max fitness={}, avg fitness={}'.format(g,
                                                                          f, self.avg_fitness()))
             if f == math.inf:
@@ -271,29 +310,15 @@ class GeneticAlgorithm:
 
         return self.get_fittest()
 
-    def crossover(self, timetable1: TimeTable, timetable2: TimeTable):
-        # Randomly copy over some genes from either chromosome and return a new one
-        timetable_crossed = TimeTable(
-            timetable1.day_list, timetable1.class_timings_list, timetable1.std_grp_list, timetable1.room_list, timetable1.subject_list, timetable1.teacher_list)
-        for i in range(timetable1.n_days):
-            for j in range(timetable1.n_class_per_day):
-                for k in range(timetable1.n_std_grps):
-                    if bool(random.getrandbits(1)):
-                        timetable_crossed.timetable[i][j][k].room = timetable1.timetable[i][j][k].room
-                    else:
-                        timetable_crossed.timetable[i][j][k].room = timetable2.timetable[i][j][k].room
-
-                    if bool(random.getrandbits(1)):
-                        timetable_crossed.timetable[i][j][k].subject = timetable1.timetable[i][j][k].subject
-                    else:
-                        timetable_crossed.timetable[i][j][k].subject = timetable2.timetable[i][j][k].subject
-
-                    if bool(random.getrandbits(1)):
-                        timetable_crossed.timetable[i][j][k].teacher = timetable1.timetable[i][j][k].teacher
-                    else:
-                        timetable_crossed.timetable[i][j][k].teacher = timetable2.timetable[i][j][k].teacher
-        return timetable_crossed
-
 
 # ga = GeneticAlgorithm(5, 8)
-# fittest = ga.run_algorithm(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri'], ['9-10', '10-11', '11-12', '12-13', '13-14', '14-15', '15-16', '16-17'], ['A', 'B', 'C'], ['math','science', 'social', 'history', 'english', 'hindi', 'computers'], ['400', '401', '402', '404', '405'], ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10'])
+# fittest = ga.run_algorithm(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri'],
+#                            ['9-10', '10-11', '11-12', '12-13', '13-14', '14-15', '15-16', '16-17'],
+#                            ['math', 'science', 'social', 'history', 'english', 'hindi', 'computers'],
+#                            ['400', '401'], # List of rooms
+#                            ['T1', 'T2', 'T3', 'T4', 'T5', 'T6'], # List of teachers
+#                            [{'math': 'T1', 'science': 'T2', 'social': 'T3', 'history': 'T4', 'english': 'T5',
+#                              'hindi': 'T6', 'computers': 'T3', 'empty': '-'},
+#                             {'math': 'T1', 'science': 'T2', 'social': 'T3', 'history': 'T4', 'english': 'T5', # List
+#                              'hindi': 'T6', 'computers': 'T3', 'empty': '-'}])
+# print(fittest.timetable)
