1d5a9a669822ec80ba94fcd280b771bac6ab8350
Aftaab Zia <aftaab1999@gmail.com>
2020-06-10 21:35:36 +0530

    Added frontend code to heroku

---
+++ frontend/precache-manifest.4419b29d516bd80719420b8097eeb0ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402c8ddb59938b4a0a68a43124370f6a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "383dc28ad20cc360d290",
+    "url": "./static/css/2.8e5c1249.chunk.css"
+  },
+  {
+    "revision": "ae93e2937faadace01eb",
+    "url": "./static/css/main.dc4a8a5f.chunk.css"
+  },
+  {
+    "revision": "383dc28ad20cc360d290",
+    "url": "./static/js/2.381d9184.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.381d9184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae93e2937faadace01eb",
+    "url": "./static/js/main.7dc43f15.chunk.js"
+  },
+  {
+    "revision": "e3d365db4d3f57fa6f46",
+    "url": "./static/js/runtime-main.96b9d70c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
